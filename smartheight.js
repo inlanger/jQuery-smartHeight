ec28fa998b22182a88112fe4f0151c72581139eb
inlanger <inlanger@gmail.com>
2012-12-25 15:27:41 +0200

    Add items per row and offset parameters. Thanks to miXOnIN

--- smartheight.js
+++ smartheight.js
@@ -1,33 +1,36 @@
 /*
 Name: smartHeight
-Author: inlanger www.inlanger.org.ua
+Author: inlanger and miXOnIN
 Description: Make similar height of all selected elements
-Usage: $(YOUR_SELECTOR).smartHeight({change: "css_property_name"});
+Usage: $(YOUR_SELECTOR).smartHeight({change: "css_property_name", tabs: 4});
 Requirements: jQuery 1.5+
 Browser support: IE 7+, Chrome 2+, FireFox 2+, Opera 9+, Safari 4+
 */
 
 (function($) {
     $.fn.smartHeight = function(options) {
-        var defaults = {
-            change: 'min-height'
-        }, opts = $.extend(defaults, options);
+      var defaults = {
+          change: 'min-height',
+          tabs: 3,
+          fix_px: 0                    
+      }, opts = $.extend(defaults, options);
 
-        function find_highest(element) {
+      function sel_highest_tr(element) {
+        if(element.length>0){
+          for(var i=0;i<Math.floor(element.length/opts.tabs);i++){
             var value = 0;
-            var t_elem;
-            $(element).each(function() {
-                $this = $(this);
-                if ($this.outerHeight() > value) {
-                    t_elem = this;
-                    value = $this.outerHeight();
-                }
+            var _tmp=$(element).filter((i==0?'*':':gt('+((i*opts.tabs)-1)+')')).filter(':lt('+opts.tabs+')');
+            _tmp.each(function() {
+              $this = $(this);
+              if ($this.outerHeight() > value) {
+                value = $this.outerHeight();
+              }
             });
-            return value;
+            _tmp.css(opts['change'], (value+opts.fix_px)+'px');
+          }
+          
         }
-        max_high = find_highest(this);
-        $(this).each(function() {
-            $(this).css(opts['change'], max_high);
-        });
+      }
+      sel_highest_tr(this);
     };
 })(jQuery);
